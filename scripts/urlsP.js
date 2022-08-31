a8bccd9636c226faf7bfd7a2df12b255da27d8bb
Estudent0010 <112423014+Estudent0010@users.noreply.github.com>
2022-08-31 15:42:25 -0300

    Add files via upload

---
+++ scripts/urlsP.js
@@ -0,0 +1,20 @@
+// barbearia link do site
+let barbeariaLink = document.getElementById('barbearia');
+barbeariaLink.addEventListener('click', () => {
+    open('https://estudent0010.github.io/barbearia/','_blank');
+});
+
+let freewayLink = document.getElementById('freeway');
+freewayLink.addEventListener('click', () => {
+    open('https://editor.p5js.org/FocaXZ/full/WXWNWMHqu','_blank');
+});
+
+let pongLink = document.getElementById('pong');
+pongLink.addEventListener('click', () => {
+    open('https://editor.p5js.org/FocaXZ/full/hm63HMN4C','_blank');
+});
+
+let flexboxLink = document.getElementById('flexbox');
+flexboxLink.addEventListener('click', () => {
+    open('https://estudent0010.github.io/alurinha/','_blank');
+});
